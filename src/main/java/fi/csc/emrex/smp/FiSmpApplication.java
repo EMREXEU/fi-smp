162016d410dde1cf6e062ab99257881cad422997
Marko Hollanti <mhollanti@gmail.com>
2015-08-25 12:57:50 +0300

    Initial commit

---
+++ src/main/java/fi/csc/emrex/smp/FiSmpApplication.java
@@ -0,0 +1,12 @@
+package fi.csc.emrex.smp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiSmpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiSmpApplication.class, args);
+    }
+}

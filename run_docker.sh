6397875bebb49ff28d37d4f88d3c52bf87f2c9cd
Tomi Tolvanen <Tomi.Tolvanen.Gofore@maakunta.cloud>
2020-11-18 08:37:12 +0200

    Fix typo.

--- run_docker.sh
+++ run_docker.sh
@@ -1,8 +1,8 @@
 docker rm fi_smp_server
-docker volume create --name=fi_np_server_volume
+docker volume create --name=fi_smp_server_volume
 docker build --tag=fi_smp_server:latest --rm=true .
 docker run \
   --name=fi_smp_server \
   --publish=9002:9002 \
-  --volume=fi_np_server_volume:/var/lib/fi_smp_server \
+  --volume=fi_smp_server_volume:/var/lib/fi_smp_server \
   fi_smp_server:latest
